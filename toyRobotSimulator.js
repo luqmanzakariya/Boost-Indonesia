a43d330dc75a7d43810568ad28dfcd65f7396c97
Luqman <luqman.zakariya@gmail.com>
2019-10-01 04:05:30 +0700

    add validation if out of board, add change direction to right, add more test case

--- toyRobotSimulator.js
+++ toyRobotSimulator.js
@@ -12,6 +12,10 @@ for (let i=0; i<5; i++){
 
 // Place Toy Robot in Board
 function place (x,y,direction){
+  if (x>4 || y>4){
+    return console.log('Out of board range. X and Y should below 5')
+  }
+
   if (direction === 'north'){
     direction = '▲'
   }
@@ -23,6 +27,9 @@ function place (x,y,direction){
   }
   else if (direction === 'west'){
     direction = '◄'
+  }
+  if (x>4 || y>4){
+    return console.log('Out of board range. X and Y should below 5')
   }
 
   for (let i=0; i<board.length; i++){
@@ -41,28 +48,28 @@ function place (x,y,direction){
 function move (){
   for (let i=0; i<board.length; i++){
     for (let j=0; j<board[i].length; j++){
-      if (board[i][j] === '▲'){
+      if (board[i][j] === '▲' && i-1>=0){
         board[i][j] = ' '
         position.x = i-1
         position.y = j
         position.f = '▲'
         position.face = 'north'
       }
-      else if (board[i][j] === '►'){
+      else if (board[i][j] === '►' && j+1<board.length){
         board[i][j] = ' '
         position.x = i
         position.y = j+1
         position.f = '►'
         position.face = 'east'
       }
-      else if (board[i][j] === '▼'){
+      else if (board[i][j] === '▼' && i+1<board.length){
         board[i][j] = ' '
         position.x = i+1
         position.y = j
         position.f = '▼'
         position.face = 'south'
       }
-      else if (board[i][j] === '◄'){
+      else if (board[i][j] === '◄' && j-1>=0){
         board[i][j] = ' '
         position.x = i
         position.y = j-1
@@ -76,6 +83,28 @@ function move (){
 }
 
 // Change Direction
+function right(){
+  for (let i=0; i<5; i++){
+    for (let j=0; j<5; j++){
+      if (board[i][j] !== ' ' && board[i][j] === '▲'){
+        board[i][j] = '►'
+        position.face = 'east'
+      }
+      else if (board[i][j] !== ' ' && board[i][j] === '►'){
+        board[i][j] = '▼'
+        position.face = 'south'
+      }
+      else if (board[i][j] !== ' ' && board[i][j] === '▼'){
+        board[i][j] = '◄'
+        position.face = 'west'
+      }
+      else if (board[i][j] !== ' ' && board[i][j] === '◄'){
+        board[i][j] = '▲'
+        position.face = 'north'
+      }
+    }
+  }
+}
 function left(){
   for (let i=0; i<5; i++){
     for (let j=0; j<5; j++){
@@ -117,18 +146,33 @@ function report (board){
   position.face = ''
 }
 
-// ========== Put command here! ==========
+
+// ========== unit tests/specs.! ==========
 place(0,0, 'north')
 move()
-report(board)
+report(board) // Expected current position: 3, 0, north
 
 place(0,0, 'north')
 left()
-report(board)
+report(board) // Expected current position: 0, 0, west
 
 place(1,2, 'east')
 move()
 move()
 left()
 move()
+report(board) // Expected current position: 2, 4, north
+
+place (5,5, 'north') //Expected Out of board range. X and Y should below 5
+
+place(3,2, 'south')
+move()
+move()
+right()
+move()
+move()
+move() //Should handle, so the positionn still in 0,1
+left()
+move()
+move() //Should handle, so the positionn still in 0,0
 report(board)
